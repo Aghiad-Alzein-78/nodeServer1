db77b1c45502dd76faa70b9dfc9260ebc18a0101
Aghiad-Alzein-78 <aghiad_alzein@yahoo.com>
2023-10-13 12:05:02 +0300

    last2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("node:http");
 const server = http.createServer((req, res) => {
     res.writeHead(200, "OK", { "content-type": "text/html" });
-    res.end("<h1>Hello World!!</h1>");
+    res.end("<h1>Hello World!! last</h1>");
 });
 
 server.listen(8000, () => {

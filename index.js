4092d356b3bfd364a13cf7a9875e687278f0d278
Aghiad-Alzein-78 <aghiad_alzein@yahoo.com>
2023-10-13 11:43:58 +0300

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("node:http");
+const server = http.createServer((req, res) => {
+    res.writeHead(200, "OK", { "content-type": "text/html" });
+    res.end("<h1>Hello World!!</h1>");
+});
+
+server.listen(8000, () => {
+    console.log("Up and running on port 8000");
+});
